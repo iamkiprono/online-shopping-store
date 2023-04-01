b2b1e81850ef25e65965208879a2c62fe8e2ebbd
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2023-04-01 13:53:04 +0300

    Updates

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
+import React from "react";
 import { FiShoppingCart } from "react-icons/fi";
 
-const Card = ({title, image, price, description}) => {
+const Card = ({ title, image, price, description }) => {
   return (
-    <div className='card'>
-        <img src={image} alt="" />
-        <h4>{title}</h4>
-        <h3>${price}</h3>
-        <p>${description}</p>
-        <div className='btn'>
-        
-        <FiShoppingCart/>
-        </div>
-
+    <div className="card">
+      <img src={image} alt="" />
+      <h4>{title}</h4>
+      <h3>${price}</h3>
+      <p>{description}</p>
+      <div className="btn">
+        <FiShoppingCart />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
